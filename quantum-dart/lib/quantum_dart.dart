1db6f0112b9fd7f56a3b001db841ea90c120a9ed
Daniel Mills <danielmillst@gmail.com>
2021-06-06 20:26:43 -0400

    Initial

---
+++ quantum-dart/lib/quantum_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library quantum_dart;
+
+export 'src/quantum_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
